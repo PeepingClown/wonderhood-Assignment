a1befd871524dbc6dfa4a1073c92b0c7fb67d040
Mayanka Chattopadhyay <jodu892@gmail.com>
2020-11-29 14:20:34 +0530

    Added Modal

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ for(var i=0;i<n;i++){
     })
 }
 
+var modal = document.getElementById("myModal");
+
+
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+for(var i=0;i<img.length;i++){
+    img[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      }
+}
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}

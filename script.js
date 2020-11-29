9b27f07a8d09e257228ecf088f49d55d0aa919ea
Mayanka Chattopadhyay <jodu892@gmail.com>
2020-11-29 17:39:57 +0530

    changes with modal and remove background

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 //     $("body").css("background-image","url('./images/first.jpg')");
 // })
 
-var n = document.querySelectorAll(".drop-i").length;
-for(var i=0;i<n;i++){
-    document.querySelectorAll(".drop-i")[i].addEventListener('click',(event) => {
-        $("body").css("background-image",`url(${event.target.getAttribute("src")})`);
-    })
-}
+// var n = document.querySelectorAll(".drop-i").length;
+// for(var i=0;i<n;i++){
+//     document.querySelectorAll(".drop-i")[i].addEventListener('click',(event) => {
+//         $("body").css("background-image",`url(${event.target.getAttribute("src")})`);
+//     })
+// }
 
 var modal = document.getElementById("myModal");
 
